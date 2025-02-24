93ffc9f43ac93e905f15efe26c564cfd40fb9038
dayeeen <nubicodes@gmail.com>
2025-02-24 22:02:11 +0700

    Add soft deletes and fillable properties to model classes

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $fillable = [
+        'name',
+        'slug',
+        'path_trailer',
+        'about',
+        'thumbnail',
+        'teacher_id',
+        'category_id',
+    ];
+
 }
--- app/Models/SubscribeTransaction.php
+++ app/Models/SubscribeTransaction.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SubscribeTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'total_amount',
+        'is_paid',
+        'proof',
+        'user_id',
+        'subscription_start_date'
+    ];
 }
