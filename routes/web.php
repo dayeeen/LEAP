8ff4bb7acdef561be1d97cd3bd3e7e965fc1f6b3
dayeeen <nubicodes@gmail.com>
2025-02-27 18:02:38 +0700

    Add FrontController and views for front-end index and details pages

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,14 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\SubscribeTransactionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[FrontController::class, 'index'])->name('front.index');
+Route::get('/details/{course:slug}',[FrontController::class, 'details'])->name('front.details');
+
+Route::get('/category/{category:slug}',[FrontController::class, 'category'])->name('front.category');
+Route::get('/pricing',[FrontController::class, 'pricing'])->name('front.pricing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -19,6 +22,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Login dulu sebelum transaksi
+    Route::get('/checkout', [FrontController::class, 'checkout'])->name('front.checkout');
+    Route::get('/checkout/store', [FrontController::class, 'checkout_store'])->name('front.checkout.store');
 
     Route::prefix('admin')->name('admin.')->group(function() {
         Route::resource('categories', CategoryController::class)
